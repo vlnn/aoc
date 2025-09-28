807ac48ae9e62c724a322ddcf7b8d3a406f30f04
vlnn <vlnn-github@proton.me>
2025-09-28 18:59:28 +0300

    Start day 2 of 2015

---
+++ src/aoc/year_2015/day_02.py
@@ -0,0 +1,32 @@
+from aoc.io import read_input
+from itertools import accumulate
+
+
+raw_input = read_input(2015, 2)
+
+
+def parse_input(raw_data: str) -> list[tuple[int, int, int]]:
+    lines = raw_data.strip().split("\n")
+    return [parse_line(line) for line in lines]
+
+
+def parse_line(line: str) -> tuple[int, int, int]:
+    return [int(x) for x in line.split("x")]
+
+
+def solve_part1(xyzs) -> int:
+    areas = [(x * y, y * z, x * z) for x, y, z in xyzs]
+    areas = [2 * s1 + 2 * s2 + 2 * s3 + min(s1, s2, s3) for s1, s2, s3 in areas]
+    return sum(areas)
+
+
+def solve_part2(xyz) -> int:
+    return 0
+
+
+def part1() -> int:
+    return solve_part1(parse_input(raw_input))
+
+
+def part2() -> int | None:
+    return solve_part2(raw_input)
